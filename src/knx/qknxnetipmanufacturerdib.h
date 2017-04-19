bb733229390b7dec4a84373e012a29c71c9efeb5
Karsten Heimrich <karsten.heimrich@qt.io>
2017-04-19 17:44:45 +0200

    Add missing default template argument
    Change-Id: Ice047269f3ce20807351ef9d5f148097230f4289
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- src/knx/qknxnetipmanufacturerdib.h
+++ src/knx/qknxnetipmanufacturerdib.h
@@ -36,7 +36,7 @@ public:
 
     QKnxNetIp::DescriptionType descriptionType() const;
     quint16 manufacturerId() const;
-    template <typename T> auto manufacturerData() const -> decltype(T())
+    template <typename T = std::vector<quint8>> auto manufacturerData() const -> decltype(T())
     {
         static_assert(is_type<T, QByteArray, QVector<quint8>, std::deque<quint8>,
             std::vector<quint8>>::value, "Type not supported.");
