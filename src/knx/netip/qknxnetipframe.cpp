9129db5f29acea28f2655df8e6443771ad2dc903
Karsten Heimrich <karsten.heimrich@qt.io>
2018-03-07 17:21:24 +0100

    Add documentation for the KNXnet/IP frame class
    Change-Id: I73ed1290e5f1d94482f710e302c5cbd4b4373ac5
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- src/knx/netip/qknxnetipframe.cpp
+++ src/knx/netip/qknxnetipframe.cpp
@@ -45,27 +45,40 @@ public:
 
     \inmodule QtKnx
     \brief The QKnxNetIpFrame class represents the base for all KNXnet/IP
-    frames.
+    related communication.
+
+    The communication between KNXnet/IP devices is based on KNXnet/IP frames.
+    A KNXnet/IP frame is a data packet sent over the non-KNX network protocol
+    that consists of a header, comparable to the IP header of an Internet
+    protocol data packet, and optional data of variable length. The type of the
+    KNXnet/IP frame is described by a KNXnet/IP service type identifier in the
+    header.
 */
 
 /*!
-    Constructs an empty invalid frame object.
-
-    \sa isNull, isValid
+    Constructs an empty invalid KNXnet/IP frame object.
 */
 QKnxNetIpFrame::QKnxNetIpFrame()
     : d_ptr(new QKnxNetIpFramePrivate)
 {}
 
 /*!
-    Destroys the frame object and releases all allocated resources.
+    Destroys the KNXnet/IP frame object and releases all allocated resources.
 */
 QKnxNetIpFrame::~QKnxNetIpFrame() = default;
 
+/*!
+    Creates a new KNXnet/IP frame with the given service type \a type and data
+    set to \a data.
+*/
 QKnxNetIpFrame::QKnxNetIpFrame(QKnxNetIp::ServiceType type, const QKnxByteArray &data)
     : QKnxNetIpFrame(type, {}, data)
 {}
 
+/*!
+    Creates a new KNXnet/IP frame with the given service type \a type,
+    connection header set to \a connectionHeader, and data set to \a data.
+*/
 QKnxNetIpFrame::QKnxNetIpFrame(QKnxNetIp::ServiceType type,
         const QKnxNetIpConnectionHeader &connectionHeader, const QKnxByteArray &data)
     : d_ptr(new QKnxNetIpFramePrivate)
@@ -75,6 +88,10 @@ QKnxNetIpFrame::QKnxNetIpFrame(QKnxNetIp::ServiceType type,
     d_ptr->m_header = { type, quint16(connectionHeader.size() + data.size()) };
 }
 
+/*!
+    Creates a new KNXnet/IP frame with the given frame header \a header,
+    connection header set to \a connectionHeader, and data set to \a data.
+*/
 QKnxNetIpFrame::QKnxNetIpFrame(const QKnxNetIpFrameHeader &header,
         const QKnxNetIpConnectionHeader &connectionHeader, const QKnxByteArray &data)
     : d_ptr(new QKnxNetIpFramePrivate)
@@ -84,11 +101,21 @@ QKnxNetIpFrame::QKnxNetIpFrame(const QKnxNetIpFrameHeader &header,
     d_ptr->m_data = data;
 }
 
+/*!
+    Returns \c true if this is a default constructed frame, otherwise returns
+    \c false. A frame is considered null if it contains no initialized values.
+*/
 bool QKnxNetIpFrame::isNull() const
 {
     return d_ptr->m_header.isNull() && d_ptr->m_connectionHeader.isNull() && d_ptr->m_data.isNull();
 }
 
+/*!
+    Returns \c true if the frame contains initialized values and is in itself
+    valid, otherwise returns \c false. A valid KNXnet/IP frame consist of
+    at least a valid header and a size in bytes corresponding to the KNXnet/IP
+    frame header \l QKnxNetIpHeader::totalSize.
+*/
 bool QKnxNetIpFrame::isValid() const
 {
     if (isNull())
@@ -111,46 +138,78 @@ bool QKnxNetIpFrame::isValid() const
         && d_ptr->m_header.dataSize() == (d_ptr->m_data.size() + d_ptr->m_connectionHeader.size());
 }
 
+/*!
+    Returns the size in bytes of the KNXnet/IP frame. The size includes the
+    size of the KNXnet/IP frame header, the optional connection header, and
+    the frame's data part.
+*/
 quint16 QKnxNetIpFrame::size() const
 {
     return d_ptr->m_header.totalSize();
 }
 
+/*!
+    Returns the size of the KNXnet/IP frame data. The data size excludes the
+    size of the KNXnet/IP frame header and the optional connection header.
+*/
 quint16 QKnxNetIpFrame::dataSize() const
 {
     return d_ptr->m_header.dataSize() - d_ptr->m_connectionHeader.size();
 }
 
+/*!
+    Returns the KNXnet/IP frame header.
+*/
 QKnxNetIpFrameHeader QKnxNetIpFrame::header() const
 {
     return d_ptr->m_header;
 }
 
+/*!
+    Sets the KNXnet/IP frame header to \a header.
+*/
 void QKnxNetIpFrame::setHeader(const QKnxNetIpFrameHeader & header)
 {
     d_ptr->m_header = header;
 }
 
+/*!
+    Returns the KNXnet/IP frame protocol version.
+*/
 quint8 QKnxNetIpFrame::protocolVersion() const
 {
     return d_ptr->m_header.protocolVersion();
 }
 
+/*!
+    Returns the KNXnet/IP frame's service type or QKnxNetIp::Unknown for
+    a default generated frame.
+*/
 QKnxNetIp::ServiceType QKnxNetIpFrame::serviceType() const
 {
     return d_ptr->m_header.serviceType();
 }
 
+/*!
+    Sets the KNXnet/IP frame's service type to \a type.
+*/
 void QKnxNetIpFrame::setServiceType(QKnxNetIp::ServiceType type)
 {
     d_ptr->m_header.setServiceType(type);
 }
 
+/*!
+    Returns the KNXnet/IP frame's connection header.
+*/
 QKnxNetIpConnectionHeader QKnxNetIpFrame::connectionHeader() const
 {
     return d_ptr->m_connectionHeader;
 }
 
+/*!
+    Sets the KNXnet/IP frame's connection header to \a header and updates
+    the total size accordingly.
+*/
 void QKnxNetIpFrame::setConnectionHeader(const QKnxNetIpConnectionHeader &header)
 {
     quint16 dataSize = d_ptr->m_header.dataSize() - d_ptr->m_connectionHeader.size();
@@ -158,36 +217,76 @@ void QKnxNetIpFrame::setConnectionHeader(const QKnxNetIpConnectionHeader &header
     d_ptr->m_header.setDataSize(dataSize + header.size());
 }
 
+/*!
+    Returns the KNXnet/IP frame's channel ID if a connection header is set.
+
+    \note By default the function returns null.
+*/
 quint8 QKnxNetIpFrame::channelId() const
 {
     return d_ptr->m_connectionHeader.channelId();
 }
 
+/*!
+    Returns the KNXnet/IP frame's sequence number if a connection header is set.
+
+    \note By default the function returns null.
+*/
 quint8 QKnxNetIpFrame::sequenceNumber() const
 {
     return d_ptr->m_connectionHeader.sequenceNumber();
 }
 
+/*!
+    Returns the service type specific value of the KNXnet/IP frame if a
+    connection header is set.
+
+    \note By default the function returns null.
+*/
 quint8 QKnxNetIpFrame::serviceTypeSpecificValue() const
 {
     return d_ptr->m_connectionHeader.serviceTypeSpecificValue();
 }
 
+/*!
+    Returns a byte array with connection type specific header items of the
+    KNXnet/IP frame if a connection header is set.
+
+    \note The returned array can be empty.
+*/
 QKnxByteArray QKnxNetIpFrame::connectionTypeSpecificHeaderItems() const
 {
     return d_ptr->m_connectionHeader.connectionTypeSpecificHeaderItems();
 }
 
+/*!
+    Returns the data part of the KNXnet/IP frame.
+
+    \note If a connection header is set, it is not considered part of the
+    returned data.
+*/
 QKnxByteArray QKnxNetIpFrame::data() const
 {
     return d_ptr->m_data;
 }
 
+/*!
+    Returns the data part of the KNXnet/IP frame as constant reference.
+
+    \note If a connection header is set, it is not considered part of the
+    returned data.
+*/
 const QKnxByteArray &QKnxNetIpFrame::constData() const
 {
     return d_ptr->m_data;
 }
 
+/*!
+    Sets the data part of the KNXnet/IP frame to \a data and updates the
+    total size accordingly.
+
+    \note The data part passed may not contain the connection header.
+*/
 void QKnxNetIpFrame::setData(const QKnxByteArray &data)
 {
     auto dataSize = d_ptr->m_header.dataSize() - d_ptr->m_data.size();
@@ -195,11 +294,18 @@ void QKnxNetIpFrame::setData(const QKnxByteArray &data)
     d_ptr->m_header.setDataSize(dataSize + data.size());
 }
 
+/*!
+    Returns an array of bytes that represent the KNXnet/IP frame.
+*/
 QKnxByteArray QKnxNetIpFrame::bytes() const
 {
     return d_ptr->m_header.bytes() + d_ptr->m_connectionHeader.bytes() + d_ptr->m_data;
 }
 
+/*!
+    Constructs the KNXnet/IP frame from the byte array \a bytes starting
+    at position \a index inside the array.
+*/
 QKnxNetIpFrame QKnxNetIpFrame::fromBytes(const QKnxByteArray &bytes, quint16 index)
 {
     auto header = QKnxNetIpFrameHeader::fromBytes(bytes, index);
@@ -303,7 +409,7 @@ QKnxNetIpFrame::QKnxNetIpFrame(QKnxNetIpFramePrivate &dd)
 /*!
     \relates QKnxNetIpFrame
 
-    Writes the KNX \a NetIpFrame to the \a debug stream.
+    Writes the KNXnet/IP \a frame to the \a debug stream.
 */
 QDebug operator<<(QDebug debug, const QKnxNetIpFrame &frame)
 {
