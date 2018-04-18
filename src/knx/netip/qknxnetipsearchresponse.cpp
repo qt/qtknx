7d12cf30dd5e0c7d009e27bd06808e002013ea90
Leena Miettinen <riitta-leena.miettinen@qt.io>
2018-04-18 13:24:28 +0200

    Doc: Add docs for the QKnxNetIpSearchResponseProxy class
    Change-Id: I7a7ff06a154e148315490178388d6d635614331f
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/knx/netip/qknxnetipsearchresponse.cpp
+++ src/knx/netip/qknxnetipsearchresponse.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 **
-** Copyright (C) 2017 The Qt Company Ltd.
+** Copyright (C) 2018 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtKnx module.
@@ -31,39 +31,162 @@
 
 QT_BEGIN_NAMESPACE
 
+/*!
+    \class QKnxNetIpSearchResponseProxy
+
+    \inmodule QtKnx
+    \brief The QKnxNetIpSearchResponseProxy class provides the means to read a
+    search response from the generic \l QKnxNetIpFrame class and to create a
+    KNXnet/IP frame based on the information.
+
+    In networks where IP addresses are assigned at runtime, via BootP or DHCP,
+    for example, it is important to be able to discover KNXnet/IP servers within
+    a subnetwork and to establish connections to them without manual input.
+
+    A KNXnet/IP client sends a search request frame, via multicast using its
+    discovery endpoint. All KNXnet/IP servers that receive the request
+    immediately send a search response using the host address protocol
+    information (HPAI) of the client's discovery endpoint. The search response
+    frame contains the HPAI of the server's control endpoint for all further
+    communication.
+
+    In addition, a search response can contain information about the server
+    hardware and the supported service families.
+
+    In most programs, this class will not be used directly. Instead, the
+    \l QKnxNetIpTunnel or \l QKnxNetIpDeviceManagement
+    class is used to establish a functional connection to a KNXnet/IP server.
+
+    The following code sample illustrates how to read the search response
+    information sent by a KNXnet/IP server:
+
+    \code
+    QKnxNetIpFrame netIpFrame;
+    QKnxNetIpSearchResponseProxy searchResponse(netIpFrame);
+    if (searchResponse.isValid()) {
+        auto netIpHpai = searchResponse.controlEndpoint();
+        auto hardwareNetIpDib = searchResponse.deviceHardware();
+        auto supportedFamillies = searchResponse.supportedFamilies();
+        // ...
+    }
+    \endcode
+
+    \sa builder(), QKnxNetIpSearchRequestProxy
+*/
+
+/*!
+    \fn QKnxNetIpSearchResponseProxy::QKnxNetIpSearchResponseProxy()
+    \internal
+*/
+
+/*!
+    \fn QKnxNetIpSearchResponseProxy::~QKnxNetIpSearchResponseProxy()
+    \internal
+*/
+
+/*!
+    \fn QKnxNetIpSearchResponseProxy::QKnxNetIpSearchResponseProxy(const QKnxNetIpFrame &&)
+    \internal
+*/
+
+/*!
+    Constructs a proxy object to read the search response information carried
+    by the specified KNXnet/IP frame \a frame.
+*/
 QKnxNetIpSearchResponseProxy::QKnxNetIpSearchResponseProxy(const QKnxNetIpFrame &frame)
     : m_frame(frame)
 {}
 
+/*!
+    Returns the control endpoint of the KNXnet/IP server.
+*/
 QKnxNetIpHpai QKnxNetIpSearchResponseProxy::controlEndpoint() const
 {
     return QKnxNetIpHpai::fromBytes(m_frame.constData(), 0);
 }
 
+/*!
+    Returns information about the KNXnet/IP server hardware.
+*/
 QKnxNetIpDib QKnxNetIpSearchResponseProxy::deviceHardware() const
 {
     return QKnxNetIpDib::fromBytes(m_frame.constData(), 8);
 }
 
+/*!
+    Returns information about the service families that the KNXnet/IP server
+    supports.
+*/
 QKnxNetIpDib QKnxNetIpSearchResponseProxy::supportedFamilies() const
 {
     return QKnxNetIpDib::fromBytes(m_frame.constData(), 62);
 }
 
+/*!
+    Returns \c true if the frame contains initialized values and is in itself
+    valid, otherwise returns \c false. A valid KNXnet/IP frame consist of
+    at least a valid header and a size in bytes corresponding to the total size
+    of the KNXnet/IP frame header.
+
+    \sa QKnxNetIpFrameHeader::totalSize()
+*/
 bool QKnxNetIpSearchResponseProxy::isValid() const
 {
     return m_frame.isValid() && m_frame.serviceType() == QKnxNetIp::ServiceType::SearchResponse
         && m_frame.size() >= 70;
 }
 
+/*!
+    Returns a builder object to create a KNXnet/IP search response frame.
+*/
 QKnxNetIpSearchResponseProxy::Builder QKnxNetIpSearchResponseProxy::builder()
 {
     return QKnxNetIpSearchResponseProxy::Builder();
 }
 
 
-// -- QKnxNetIpSearchResponseProxy::Builder
+/*!
+    \class QKnxNetIpSearchResponseProxy::Builder
 
+    \inmodule QtKnx
+    \brief The QKnxNetIpSearchResponseProxy::Builder class provides the means to
+    create a KNXnet/IP search response frame.
+
+    A search response frame contains the host address protocol information
+    (HPAI) of the KNXnet/IP server's control endpoint to use for communication
+    with a KNXnet/IP client.
+
+    In addition, a search response can contain information about the server
+    hardware and the supported service families.
+
+    In most programs, this class will not be used directly. Instead, the
+    \l QKnxNetIpTunnel or \l QKnxNetIpDeviceManagement
+    class is used to establish a functional connection to a KNXnet/IP server.
+
+    The common way to create a a search response is:
+
+    \code
+    QKnxNetIpHpai controlEndpoint;
+    QKnxNetIpDib deviceHardware, supportedFamillies;
+    QKnxNetIpFrame netIpFrame = QKnxNetIpSearchResponseProxy::builder()
+                                .setControlEndpoint(controlEndpoint)
+                                .setSupportedFamilies(deviceHardware)
+                                .setDeviceHardware(supportedFamillies)
+                                .create();
+    \endcode
+
+    \sa QKnxNetIpHpaiProxy::builder(), QKnxNetIpServiceFamiliesDibProxy::builder(), QKnxNetIpDeviceDibProxy::builder()
+
+    Typically, after discovering a KNXnet/IP server, the KNXnet/IP client sends
+    a description request, \l QKnxNetIpDescriptionRequestProxy, through a
+    unicast or point-to-point connection to all control endpoints of the
+    KNXnet/IP server.
+*/
+
+/*!
+    Sets the control endpoint of the KNXnet/IP server to \a hpai and returns a
+    reference to the builder.
+*/
 QKnxNetIpSearchResponseProxy::Builder &
     QKnxNetIpSearchResponseProxy::Builder::setControlEndpoint(const QKnxNetIpHpai &hpai)
 {
@@ -71,6 +194,10 @@ QKnxNetIpSearchResponseProxy::Builder &
     return *this;
 }
 
+/*!
+    Sets the KNXnet/IP server device information block (DIB) to \a ddib and
+    returns a reference to the builder.
+*/
 QKnxNetIpSearchResponseProxy::Builder &
     QKnxNetIpSearchResponseProxy::Builder::setDeviceHardware(const QKnxNetIpDib &ddib)
 {
@@ -79,6 +206,10 @@ QKnxNetIpSearchResponseProxy::Builder &
     return *this;
 }
 
+/*!
+    Sets the device families supported by the KNXnet/IP server to \a sdib and
+    returns a reference to the builder.
+*/
 QKnxNetIpSearchResponseProxy::Builder &
     QKnxNetIpSearchResponseProxy::Builder::setSupportedFamilies(const QKnxNetIpDib &sdib)
 {
@@ -87,6 +218,14 @@ QKnxNetIpSearchResponseProxy::Builder &
     return *this;
 }
 
+/*!
+    Creates and returns a KNXnet/IP search response frame.
+
+    \note The returned frame may be invalid depending on the values used during
+    setup.
+
+    \sa isValid()
+*/
 QKnxNetIpFrame QKnxNetIpSearchResponseProxy::Builder::create() const
 {
     return { QKnxNetIp::ServiceType::SearchResponse, m_hpai.bytes() + m_ddib.bytes() + m_sdib
