7d7cd900ad815f0a3b2c86391ed81bfe2aa1471d
Leena Miettinen <riitta-leena.miettinen@qt.io>
2018-04-18 09:52:30 +0200

    Doc: Add docs for the QKnxNetIpDisconnectRequestProxy class
    Change-Id: Ia4968e0149c9709202deb3ec26efa9084d9d12be
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/knx/netip/qknxnetipdisconnectrequest.cpp
+++ src/knx/netip/qknxnetipdisconnectrequest.cpp
@@ -31,34 +31,152 @@
 
 QT_BEGIN_NAMESPACE
 
+/*!
+    \class QKnxNetIpDisconnectRequestProxy
+
+    \inmodule QtKnx
+    \brief The QKnxNetIpDisconnectRequestProxy class provides the means to read
+    a KNXnet/IP disconnection request from the generic \l QKnxNetIpFrame
+    class and to create a disconnection request frame based on the information.
+
+    A KNXnet/IP client terminates a data channel connection to a KNXnet/IP
+    server by sending a disconnection request to the server's control endpoint.
+    The client should try to disconnect gracefully, even under error conditions.
+
+    The server may disconnect from the client by sending a disconnection
+    request due to internal problems or reception of invalid data packets.
+    However, it is recommended to let the client terminate the connection.
+
+    The KNXnet/IP device receiving the disconnection request acknowledges the
+    operation with a KNXnet/IP disconnection response frame,
+    \l QKnxNetIpDisconnectResponseProxy, which signals the final termination
+    of a communication channel.
+
+    In most programs, this class will not be used directly. Instead, the
+    \l QKnxNetIpTunnel or \l QKnxNetIpDeviceManagement
+    class is used to establish a functional connection to a KNXnet/IP server.
+
+    The following code sample illustrates how to read the disconnection request
+    information sent by a KNXnet/IP client:
+
+    \code
+    QKnxNetIpFrame netIpFrame;
+    QKnxNetIpDisconnectRequestProxy disconnectRequest(netIpFrame);
+    if (!disconnectRequest.isValid())
+        return;
+    quint8 chanId = disconnectRequest.channelId();
+    QKnxNetIpHpai ctrlEndPoint = disconnectRequest.controlEndpoint();
+    // ...
+    \endcode
+
+    \sa builder()
+*/
+
+/*!
+    \fn QKnxNetIpDisconnectRequestProxy::QKnxNetIpDisconnectRequestProxy()
+    \internal
+*/
+
+/*!
+    \fn QKnxNetIpDisconnectRequestProxy::~QKnxNetIpDisconnectRequestProxy()
+    \internal
+*/
+
+/*!
+    \fn QKnxNetIpDisconnectRequestProxy::QKnxNetIpDisconnectRequestProxy(const QKnxNetIpFrame &&)
+    \internal
+*/
+
+/*!
+    Constructs a proxy object to read the disconnection request information
+    carried by the specified KNXnet/IP frame \a frame.
+*/
 QKnxNetIpDisconnectRequestProxy::QKnxNetIpDisconnectRequestProxy(const QKnxNetIpFrame &frame)
     : m_frame(frame)
 {}
 
+/*!
+    Returns the ID of the communication channel between the KNXnet/IP client and
+    server.
+*/
 quint8 QKnxNetIpDisconnectRequestProxy::channelId() const
 {
     return m_frame.constData().value(0);
 }
 
+/*!
+    Returns the control endpoint of the KNXnet/IP device sending the
+    disconnection request.
+*/
 QKnxNetIpHpai QKnxNetIpDisconnectRequestProxy::controlEndpoint() const
 {
     return QKnxNetIpHpai::fromBytes(m_frame.constData(), 2);
 }
 
+/*!
+    Returns \c true if the frame contains initialized values and is in itself
+    valid, otherwise returns \c false. A valid KNXnet/IP frame consist of
+    at least a valid header and a size in bytes corresponding to the total size
+    of the KNXnet/IP frame header.
+
+    \sa QKnxNetIpFrameHeader::totalSize()
+*/
 bool QKnxNetIpDisconnectRequestProxy::isValid() const
 {
     return m_frame.isValid() && m_frame.size() == 16
          && m_frame.serviceType() == QKnxNetIp::ServiceType::DisconnectRequest;
 }
 
+/*!
+    Returns a builder object to create a KNXnet/IP disconnection request frame.
+*/
 QKnxNetIpDisconnectRequestProxy::Builder QKnxNetIpDisconnectRequestProxy::builder()
 {
     return QKnxNetIpDisconnectRequestProxy::Builder();
 }
 
 
-// -- QKnxNetIpDisconnectRequestProxy::Builder
+/*!
+    \class QKnxNetIpDisconnectRequestProxy::Builder
 
+    \inmodule QtKnx
+    \brief The QKnxNetIpDisconnectRequestProxy::Builder class provides the means
+    to read a KNXnet/IP disconnection request from the generic \l QKnxNetIpFrame
+    class and to create a disconnection request frame based on the information.
+
+    A KNXnet/IP client terminates a data channel connection to a KNXnet/IP
+    server by sending a disconnection request to the server's control endpoint.
+    The client should try to disconnect gracefully, even under error conditions.
+
+    The server may disconnect from the client by sending a disconnection
+    request due to internal problems or reception of invalid data packets.
+    However, it is recommended to let the client terminate the connection.
+
+    The KNXnet/IP device receiving the disconnection request acknowledges the
+    operation with a KNXnet/IP disconnection response frame,
+    \l QKnxNetIpDisconnectResponseProxy, which signals the final termination of
+    a communication channel.
+
+    In most programs, this class will not be used directly. Instead, the
+    \l QKnxNetIpTunnel or \l QKnxNetIpDeviceManagement
+    class is used to establish a functional connection to a KNXnet/IP server.
+
+    The common way to create a disconnection request is:
+
+    \code
+    QKnxNetIpFrame netIpFrame = QKnxNetIpDisconnectRequestProxy::builder()
+                                .setChannelId(200)
+                                .setControlEndpoint(QKnxNetIpHpaiProxy::builder()
+                                    .setHostAddress(QHostAddress::LocalHost)
+                                    .setPort(3671).create())
+                                .create();
+    \endcode
+*/
+
+/*!
+    Sets the ID of the communication channel between the KNXnet/IP client and
+    server to \a channelId and returns a reference to the builder.
+*/
 QKnxNetIpDisconnectRequestProxy::Builder &
     QKnxNetIpDisconnectRequestProxy::Builder::setChannelId(quint8 channelId)
 {
@@ -66,6 +184,10 @@ QKnxNetIpDisconnectRequestProxy::Builder &
     return *this;
 }
 
+/*!
+    Sets the control endpoint of the KNXnet/IP device sending the disconnection
+    request to \a hpai and returns a reference to the builder.
+*/
 QKnxNetIpDisconnectRequestProxy::Builder &
     QKnxNetIpDisconnectRequestProxy::Builder::setControlEndpoint(const QKnxNetIpHpai &hpai)
 {
@@ -73,6 +195,14 @@ QKnxNetIpDisconnectRequestProxy::Builder &
     return *this;
 }
 
+/*!
+    Creates and returns a KNXnet/IP disconnection request frame.
+
+    \note The returned frame may be invalid depending on the values used during
+    setup.
+
+    \sa isValid()
+*/
 QKnxNetIpFrame QKnxNetIpDisconnectRequestProxy::Builder::create() const
 {
     return { QKnxNetIp::ServiceType::DisconnectRequest, QKnxByteArray { m_channelId, 0x00 }
